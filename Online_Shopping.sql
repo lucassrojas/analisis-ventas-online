33e70e45828195d451e7dd51d190ec7235ea00db
lucassrojas <97802576+lucassrojas@users.noreply.github.com>
2026-02-12 17:23:35 -0300

    Add files via upload

---
+++ Online_Shopping.sql
@@ -0,0 +1,116 @@
+-- Proyecto: Análisis de Online Shopping
+-- Dataset: Online Shopping Dataset
+-- Link: https://www.kaggle.com/datasets/jacksondivakarr/online-shopping-dataset
+-- Autor: Lucas Rojas
+
+
+
+
+select top 100 * from dbo.[file]
+
+-- Limpieza de Datos
+
+-- Creo una copia de la tabla original, donde realizaré la limpieza. La copia contendrá solo las variables importantes para el análisis
+
+select cast(cast(CustomerID as float) AS int) as CustomerID, Gender, Location, cast(cast(Transaction_ID as float) as int) as Transaction_ID, Transaction_Date, Product_Category, cast(cast(Quantity as float) as int) as Quantity, cast(Avg_Price AS decimal(10,2)) as Avg_Price into file2 from dbo.[file]
+
+select top 100 * from file2
+
+alter table file2 alter column CustomerID varchar(250) --Cambio CustomerID a texto
+
+update file2 set CustomerID = trim(CustomerID), Gender = trim(Gender), Location = trim(Location), Product_Category = trim(Product_Category) --remover espacios en blanco de las columnas de texto
+
+--Detecto valores nulos (31) y elimino
+
+select * from file2 where CustomerID is null or Gender is null or Location is null or Transaction_ID is null or Transaction_Date is null or Product_Category is null or Quantity is null or Avg_Price is null
+
+delete from file2 where CustomerID is null or Gender is null or Location is null or Transaction_ID is null or Transaction_Date is null or Product_Category is null or Quantity is null or Avg_Price is null
+
+--Duplicados
+
+select CustomerID,Gender,Location,Transaction_ID,Transaction_Date,Product_Category,Quantity,Avg_Price, count(*) repeticiones from file2 group by CustomerID,Gender,Location,Transaction_ID,Transaction_Date,Product_Category,Quantity,Avg_Price having count(*) > 1
+
+with repes as (select CustomerID,Gender,Location,Transaction_ID,Transaction_Date,Product_Category,Quantity,Avg_Price, count(*) repeticiones from file2 group by CustomerID,Gender,Location,Transaction_ID,Transaction_Date,Product_Category,Quantity,Avg_Price having count(*) > 1) select sum(repeticiones - 1) from repes
+
+-- Existen 4817 filas duplicadas, voy a eliminarlas
+-- Creo una columna id
+
+alter table file2 add id int identity(1,1)
+
+select count(*) from file2 -- Con duplicados, se tienen 52924 registros
+
+delete from file2 where id not in(select min(id) from file2 group by CustomerID,Gender,Location,Transaction_ID,Transaction_Date,Product_Category,Quantity,Avg_Price)
+
+alter table file2 drop column id
+
+select count(*) from file2 --Sin duplicados y sin nulls, se tienen 48107 registros
+
+-- Verifico posibles valores extraños
+
+select Gender, count(*) from file2 group by Gender
+
+select Location, count(*) from file2 group by Location
+
+select Product_Category, count(*) from file2 group by Product_Category
+
+-- No se presentan nombres raros
+
+
+
+
+
+
+
+-- Consultas de Métricas y KPIs
+
+-- Revenue total
+
+select sum(avg_price * quantity) as revenue from file2
+
+
+-- Ticket Promedio
+
+select Transaction_ID, count(*) from file2 group by Transaction_ID  -- Existen transacciones repetidas. Una sola transaccion puede ser de varios productos.
+
+select sum(avg_price * Quantity) / count(distinct(Transaction_ID)) as ticket_promedio from file2 -- Revenue Total / Total de Ventas Únicas
+
+
+
+-- Total de Clientes
+
+select count(distinct CustomerID) as clientes_totales from file2
+
+
+
+-- Unidades Vendidas
+
+Select sum(quantity) as total_unidades_vendidas from file2
+
+
+-- Revenue por mes:
+
+-- Reviso la cantidad de años registrados
+
+select year(Transaction_Date), count(*) from file2 group by year(transaction_date) -- Hay un solo año
+
+-- No hay necesidad de separar por años ya que hay uno solo
+
+select datename(month,Transaction_Date) as mes, sum(avg_price * quantity) as revenue_mes from file2 group by month(Transaction_Date),datename(month,Transaction_Date) order by month(Transaction_Date) asc
+
+
+-- Top 4 Categorías por Ventas
+
+with ventas as (select Product_Category as categoria, count(*) as ventas_categoria from file2 group by Product_Category) select top 5 *, rank() over(order by ventas_categoria desc) ranking from ventas
+
+
+-- Top 4 Clientes por Revenue
+
+with revenues as (select CustomerID, sum(Avg_Price * Quantity) as revenue_cliente from file2 group by CustomerID) select top 5 *, rank() over(order by revenue_cliente desc) ranking from revenues
+
+
+-- Distribución de Ventas por Ciudad
+
+select Location as ciudad, count(*) as ventas_ciudad from file2 group by Location
+
+with ventas as (select Location as ciudad, COUNT(*) as ventas_ciudad from file2 group by Location) select ciudad, ventas_ciudad * 100.0 / (select count(*) as ventas_totales from file2) as porcentaje from ventas
+
---
+++ README.md
@@ -0,0 +1,51 @@
+## Análisis de Online Shopping 2019
+
+**Dataset:** Online Shopping Dataset, publicado en Kaggle por Jackson Divakar R.
+
+**Link:** https://www.kaggle.com/datasets/jacksondivakarr/online-shopping-dataset.
+
+Análisis de los datos crudos de las ventas de un Shopping Online durante el año 2019, con el objetivo de comprender las tendencias más importantes de la empresa, como por ejemplo, el rendimiento de ingresos o el comportamiento de los clientes.
+
+**Herramientas Utilizadas:** SQL y Power BI.
+
+<hr style="height: 3px; background-color: #d2d7db; border: none;">
+
+![Dashboard de Ventas](Online_Shopping.png)
+
+<hr style="height: 3px; background-color: #d2d7db; border: none;">
+
+## Insights
+
+- El Revenue Total fue de $4.466.500, manteniendo un crecimiento progresivo durante la mayor parte del año.
+
+- Se observa el pico máximo entre noviembre, diciembre y enero. Seguido, se observa una caída relevante en febrero, de aproximadamente $500.000 a $300.000.
+
+- La cantidad de unidades vendidas es muy elevada respecto a la cantidad de clientes únicos, lo que sugiere pedidos de múltiples unidades.
+
+- Chicago y California generan el 65.2% de las ventas totales, y New York, con una participación relevante pero menor, presenta claras oportunidades de crecimiento.
+
+- Las líneas de producto más vendidas fueron Apparel (Ropa) y Nest-USA, las dos superando por más del doble en volumen de ventas al resto.
+
+<hr style="height: 3px; background-color: #d2d7db; border: none;">
+
+## Recomendaciones Estratégicas
+
+- Se recomienda realizar campañas focalizadas en New York, ya que representa el 21.1% del revenue total y presenta un alto potencial de crecimiento.
+
+- Implementar promociones agresivas en febrero, con el fin de moderar el descenso en las ventas respecto a fin de año.
+
+- Los clientes de IDs 12748 y 15311 tuvieron un gran impacto en el revenue total. Se recomienda priorizar la fidelidad de estos clientes a largo plazo.
+
+- Garantizar la disponibilidad constante de los productos Apparel y Nest-USA, y utilizar su alta demanda para impulsar categorías con menor volumen de ventas.
+
+<hr style="height: 3px; background-color: #d2d7db; border: none;">
+
+## Datos y Pasos Realizados
+
+**Dataset:** Se trabajó con un dataset el cual, luego del proceso de limpieza y transformación de datos, consta de unos 48.107 registros. Cabe recalcar que los valores NULL y los registros duplicados no fueron tenidos en cuenta a la hora de realizar el análisis. Los campos que se usaron fueron:
+
+CustomerID, Location (ciudades), Transaction_ID, Transaction_Date, Product_Category, Quantity y Avg_Price.
+
+La variable Gender (género) se mantuvo en el modelo, pero fue excluida de los reportes finales para priorizar métricas con mayor impacto en la toma de decisiones.
+
+**Procedimiento:** El estudio se realizó mediante la ejecucion de consultas SQL para extraer y verificar métricas de rendimiento, las cuales fueron recreadas con medidas DAX en Power BI, herramienta donde se llevó a cabo el modelado final de datos y el diseño de las visualizaciones.
